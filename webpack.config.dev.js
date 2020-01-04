8f993db35327f4ddd8335087417a11cd4cf4b26d
Rui Cui <ruicui@Ruis-MacBook-Pro.local>
2020-01-03 16:49:10 -0800

    react, babel, webpack env

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: './index.html'
+});
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: './dist/bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        htmlWebpackPlugin
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        open: true,
+        port: 3000
+    }
+}
